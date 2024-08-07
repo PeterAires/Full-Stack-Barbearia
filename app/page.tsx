1bcef744f2571ae85794464d1ea483df37f1ada7
PeterAires <168139612+PeterAires@users.noreply.github.com>
2024-08-06 21:01:00 -0300

    remove husky beacause problems

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,6 @@
-'use client'
+"use client";
 import { Button } from "./_components/ui/button";
 
-import Image from "next/image";
-import { useState } from "react";
-
 export default function Home() {
-
-  const [teste,setTeste] = useState()
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button  variant='outline' className="bg-zinc-600 text-zinc-200"
-      size='lg'>teste</Button>
-    </main>
-  );
+  return <Button>teste</Button>;
 }
