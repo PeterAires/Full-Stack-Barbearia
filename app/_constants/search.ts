d2c83526d7c1c9bd9464f06f85ae9a405b8d71a0
PeterAires <168139612+PeterAires@users.noreply.github.com>
2024-08-08 21:28:37 -0300

    feat: booking add component

---
+++ app/_constants/search.ts
@@ -0,0 +1,22 @@
+interface QuickSearchOptions{
+    imageUrl: string,
+    title: string
+  }
+ export const quickSearchOptions: QuickSearchOptions[] = [{
+    imageUrl: '/cabelo.png' ,
+    title: 'Cabelo'
+  },{
+    imageUrl: '/barba.png',
+    title: 'Barba'
+  },{
+    imageUrl: '/acabamento.png' ,
+    title: 'Acabamento'
+  },{
+    imageUrl: '/massagem.png' ,
+    title: 'Massagem'
+  },{
+    imageUrl: '/sobrancelha.png' ,
+    title: 'Sobrancelha'
+  },{imageUrl: '/hidratacao.png' ,
+    title: 'Hidratação'
+  }]
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,16 @@
 import { Input } from "./_components/ui/input";
 import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
-import { EyeIcon, FootprintsIcon, SearchIcon } from "lucide-react";
+import { SearchIcon } from "lucide-react";
 import Image from "next/image";
 import { Card, CardContent } from "./_components/ui/card";
-import { Badge } from "./_components/ui/badge";
-import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
 import { db } from "./_lib/prisma";
 import BarberShopItem from "./_components/barbershop-item";
+import { quickSearchOptions } from "./_constants/search";
+import { BookingItem } from "./_components/booking-item";
+
+
+
 
  const Home = async () => {
   //Chamar meu banco de dados
@@ -39,29 +42,12 @@ import BarberShopItem from "./_components/barbershop-item";
         </div>
         {/* Busca Rapída */}
         <div className="flex gap-3 mt-6 overflow-x-scroll [&::-webkit-scrollbar]:hidden ">
-          <Button className="gap-2" variant='secondary'>
-            <Image src='/cabelo.png' alt="Cabelo" width={16} height={16}/>
-            Cabelo
+          {quickSearchOptions.map((option) => (
+            <Button key={option.title} className="gap-2" variant='secondary'>
+            <Image src={option.imageUrl} alt={option.title} width={16} height={16}/>
+            {option.title}
           </Button>
-
-          <Button className="gap-2" variant='secondary'>
-            <Image src='/barba.png' alt="Barba" width={16} height={16}/>
-            Barba
-          </Button>
-
-          <Button className="gap-2" variant='secondary'>
-            <Image src='/acabamento.png' alt="Acbamento" width={16} height={16}/>
-            Acabamento
-          </Button>
-          <Button className="gap-2" variant='secondary'>
-            <FootprintsIcon size={16}/>
-            Pezinho
-          </Button>
-
-          <Button className="gap-2" variant='secondary'>
-            <EyeIcon/>
-            Sobrancelha
-          </Button>
+          ))}
         </div>
         {/* Imagem */}
         <div className="relative w-full h-[150px] mt-6">
@@ -70,29 +56,7 @@ import BarberShopItem from "./_components/barbershop-item";
           fill className="object-cover rounded-xl" />
         </div>
         {/* Agendamento */}
-        <h2 className="mb-3 mt-6 uppercase text-xs font-bold text-gray-400 ">Agendamento</h2>
-        <Card >
-          <CardContent className="flex justify-between p-0">
-            {/* Esquerda */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"/>
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-            {/* Direita */}
-            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid ">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem/>
 
         <h2 className="mb-3 mt-6 uppercase text-xs font-bold text-gray-400 ">
           Recomendados
