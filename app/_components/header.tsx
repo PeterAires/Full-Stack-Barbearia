323c254f4e95445986095f8856b2b1c986abbcb0
PeterAires <168139612+PeterAires@users.noreply.github.com>
2024-08-14 19:56:49 -0300

    test radix, try resolve sheet error

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,28 +1,34 @@
-'use client' 
-import { MapIcon, MenuIcon } from "lucide-react";
+"use client";
+import { MapIcon, MenuIcon, X } from "lucide-react";
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
 import Image from "next/image";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger,} from "./ui/sheet";
+import { Dialog, DialogClose, DialogContent, DialogOverlay, DialogPortal, DialogTrigger } from "@radix-ui/react-dialog";
 
 const Header = () => {
     return ( 
         <Card>
             <CardContent className="p-5 flex flex-row items-center justify-between">
                 <Image alt="FSW Barber" src='/Logo.png' height={18} width={120} />
-
-                <Sheet>
-                    <SheetTrigger>Open</SheetTrigger>
-                    <SheetContent>
-                        <SheetHeader>
-                        <SheetTitle>Are you absolutely sure?</SheetTitle>
-                        <SheetDescription>
-                            This action cannot be undone. This will permanently delete your account
-                            and remove your data from our servers.
-                        </SheetDescription>
-                        </SheetHeader>
-                    </SheetContent>
-                </Sheet>
+                <Dialog>
+                <DialogTrigger asChild>
+        <button className="open-sidebar-button">Open Sidebar</button>
+      </DialogTrigger>
+      <DialogPortal>
+        <DialogOverlay className="sidebar-overlay" />
+        <DialogContent className="sidebar-content">
+          <DialogClose className="close-button">
+            <X size={24} />
+          </DialogClose>
+          <div className="sidebar-body">
+            <h3>dadwad</h3>
+            <h2>Sidebar Title</h2>
+            <p>This is the content of the sidebar.</p>
+          </div>
+        </DialogContent>
+      </DialogPortal>
+                </Dialog>
 
             </CardContent>
         </Card>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import Footer from "./_components/footer";
+import '@radix-ui/themes/styles.css';
+import { Theme } from "@radix-ui/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>{children}
+        <Theme>
         <Toaster/>
         <Footer/>
+        </Theme>
       </body>
     </html>
   );
