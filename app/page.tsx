f82577866428d9d20ce0f7e3ff8419e0fff33c19
PeterAires <168139612+PeterAires@users.noreply.github.com>
2024-09-10 18:44:43 -0300

    feat: update welcome message.

--- app/page.tsx
+++ app/page.tsx
@@ -11,69 +11,91 @@ import Search from "./_components/search";
 import Link from "next/link";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./_lib/auth";
-
+import { ptBR } from "date-fns/locale";
+import { format } from "date-fns";
 
 const Home = async () => {
-  const session = await getServerSession(authOptions)
-  const barbershops = await db.barbershop.findMany({}); 
-  const popularBarbershops = await db.barbershop.findMany({ 
-    orderBy: { 
-      name: "desc",
-  }});
-  const confirmedBookings = session?.user ? 
-  await db.booking.findMany({ //se tiver um user, chama a função
-    where: {
-      userId: (session?.user as any).id,
-      date: {
-        gte: new Date()
-      }
-    },
-    include:{
-      service:{
-        include:{
-          barbershop: true
-        }
-      } 
-    },
+  const session = await getServerSession(authOptions);
+  const barbershops = await db.barbershop.findMany({});
+  const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
-      date: 'asc',
-    }
-  }) : []
+      name: "desc",
+    },
+  });
+  const confirmedBookings = session?.user
+    ? await db.booking.findMany({
+        //se tiver um user, chama a função
+        where: {
+          userId: (session?.user as any).id,
+          date: {
+            gte: new Date(),
+          },
+        },
+        include: {
+          service: {
+            include: {
+              barbershop: true,
+            },
+          },
+        },
+        orderBy: {
+          date: "asc",
+        },
+      })
+    : [];
 
-return (
+  return (
     <div>
       {/* Header */}
-      <Header/>
+      <Header />
       <div className="p-5">
         {/* Texto */}
+
         <h2 className="text-xl font-bold">
-          Olá, felipe
+          Olá, {session?.user ? session.user.name : "Bem vindo!"}
         </h2>
-        <p>Segunda Feira, 5 de Agosto</p>
+        <p>
+          <span className=" capitalize">
+            {format(new Date(), "EEEE, dd", { locale: ptBR })}
+          </span>
+          <span>&nbsp;de&nbsp;</span>
+          <span className=" capitalize">{format(new Date(), "MMMM", { locale: ptBR })}</span>
+        </p>
+
         {/* Busca */}
-        <div className=" mt-6">{/* para nao impactar o espaçamento do component pai */}
-          <Search/>
+        <div className=" mt-6">
+          {/* para nao impactar o espaçamento do component pai */}
+          <Search />
         </div>
         {/* Busca Rapída */}
         <div className="flex gap-3 mt-6 overflow-x-scroll [&::-webkit-scrollbar]:hidden ">
           {quickSearchOptions.map((option) => (
-            <Button key={option.title} className="gap-2" variant='secondary' asChild>
-            <Link href={`/barbershops?service=${option.title}`}>
-              <Image 
-              src={option.imageUrl} 
-              alt={option.title} 
-              width={16} 
-              height={16}/>
-              {option.title}
-            </Link>
-          </Button>
+            <Button
+              key={option.title}
+              className="gap-2"
+              variant="secondary"
+              asChild
+            >
+              <Link href={`/barbershops?service=${option.title}`}>
+                <Image
+                  src={option.imageUrl}
+                  alt={option.title}
+                  width={16}
+                  height={16}
+                />
+                {option.title}
+              </Link>
+            </Button>
           ))}
         </div>
         {/* Imagem */}
         <div className="relative w-full h-[150px] mt-6">
-          <Image 
-          alt='Agende nos melhores com FSW Barber' src='/banner-01.png'
-          fill className="object-cover rounded-xl" />
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="object-cover rounded-xl"
+          />
         </div>
         {/* Agendamento */}
         <h2 className="mb-3 mt-6 uppercase text-xs font-bold text-gray-400 ">
@@ -81,7 +103,7 @@ return (
         </h2>
         <div className=" flex overflow-x-auto gap-3  [&::-webkit-scrollbar]:hidden ">
           {confirmedBookings.map((booking) => (
-            <BookingItem key={booking.id} booking={booking}/>
+            <BookingItem key={booking.id} booking={booking} />
           ))}
         </div>
 
@@ -89,17 +111,21 @@ return (
           Recomendados
         </h2>
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden ">
-        {barbershops.map(barbershop => <BarberShopItem key={barbershop.id} barbershop={barbershop}/>)}
+          {barbershops.map((barbershop) => (
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
         </div>
 
         <h2 className="mb-3 mt-6 uppercase text-xs font-bold text-gray-400 ">
           Recomendados
         </h2>
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden ">
-        {popularBarbershops.map(barbershop => <BarberShopItem key={barbershop.id} barbershop={barbershop}/>)}
+          {popularBarbershops.map((barbershop) => (
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
         </div>
-      </div> 
+      </div>
     </div>
   );
-}
-export default Home
+};
+export default Home;
